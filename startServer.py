3474b0add0ed6f9656477691ec474385102edcf9
jaythrian <50998891+jaythrian@users.noreply.github.com>
2022-05-04 13:38:33 -0500

    Update startServer.py

--- startServer.py
+++ startServer.py
@@ -1,14 +1,4 @@
 import subprocess
 
-directory = "/Documents/Programming/RPi_Proj"
-
-
-
-
 # runs the flask server
 subprocess.run("export FLASK_APP=flaskr && export FLASK_ENV=development && flask run", shell=True)
-#subprocess.run('export FLASK_ENV=development', shell=True)
-#subprocess.run('flask run', shell=True)
-
-
-
