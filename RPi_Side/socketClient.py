9b8d95c94f61f8ec21c4906dbb3430d9c58dd388
Ian Golsby <iangolsby49@gmail.com>
2022-05-04 16:54:04 -0500

    Add files via upload

--- RPi_Side/socketClient.py
+++ RPi_Side/socketClient.py
@@ -6,6 +6,7 @@ PORT = 65432
 def sendData(data):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
+        s.setsockopt(socket.SQL_SOCKET, socket.SO_REUSEADDR, 1)
         s.sendall(data)
         stuff = s.recv(1024)
         print(f"Received {stuff!r}")
